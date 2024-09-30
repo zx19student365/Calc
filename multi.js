6b791d792f818a477384148553a98f1f121bc955
zx19student365 <zx19student365@colegiovalledelmiro.es>
2024-09-30 08:59:34 +0200

    subiendo multi

---
+++ multi.js
@@ -0,0 +1,8 @@
+function multi(){
+    let num1 = parseInt(prompt("dime un numero"));
+    let num2 = parseInt(prompt("dime el segundo numero"));
+
+    let resmulti = num1 * num2;
+    alert("El resultado de la multiplicacion es "+ resmulti);
+}
+multi();
