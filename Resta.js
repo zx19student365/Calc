a0578d4302c4ba73969b6657a19a4591013ef2c1
zx19student365 <zx19student365@colegiovalledelmiro.es>
2024-09-30 08:56:48 +0200

    subiendo resta

---
+++ Resta.js
@@ -0,0 +1,8 @@
+function resta(){
+    let num1 = parseInt(prompt("dime un numero"));
+    let num2 = parseInt(prompt("dime el segundo numero"));
+
+    let resresta = num1 - num2;
+    alert("El resultado de la resta es "+ resresta);
+}
+resta();
