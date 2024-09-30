3480c92b9ebf1313ce24adf38a2907a382b0f983
zx19student365 <zx19student365@colegiovalledelmiro.es>
2024-09-30 08:53:30 +0200

    subiendo suma

---
+++ Suma.js
@@ -0,0 +1,8 @@
+function suma(){
+    let num1 = parseInt(prompt("dime un numero"));
+    let num2 = parseInt(prompt("dime el segundo numero"));
+
+    let ressuma = num1 + num2;
+    alert("El resultado de la suma es "+ ressuma);
+}
+suma();
