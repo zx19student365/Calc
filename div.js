20c2d6e466b6a14b9ccd32a4548161cb2f884040
zx19student365 <zx19student365@colegiovalledelmiro.es>
2024-10-04 08:35:19 +0200

    Añadiendo division

---
+++ div.js
@@ -0,0 +1,8 @@
+function div(){
+    let num1 = parseInt(prompt("dime un numero"));
+    let num2 = parseInt(prompt("dime el segundo numero"));
+
+    let resdiv = num1 / num2;
+    alert("El resultado de la suma es "+ resdiv);
+}
+div();
