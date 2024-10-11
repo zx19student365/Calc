fe8d5d6a4136b66c2709cc1ec6dbb8e75a6c23cb
zx19student365 <zx19student365@colegiovalledelmiro.es>
2024-10-11 08:46:05 +0200

    Subiendo potencia

---
+++ potencias.js
@@ -0,0 +1,8 @@
+function potencia(){
+    let num1 = parseInt(prompt("dime un numero"));
+    let num2 = parseInt(prompt("dime el segundo numero"));
+
+    let potencia = num1 * num2;
+    alert("El resultado de la multiplicacion es "+ resresta);
+}
+potencia();
